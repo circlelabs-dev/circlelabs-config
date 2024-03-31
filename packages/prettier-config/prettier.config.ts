900c91d8c07b5359d8dc6eec55364e28860091ac
Jonathan Heneis <jonathan.heneis@circlelabs.dev>
2024-03-31 22:17:28 +0200

    init commit

---
+++ packages/prettier-config/prettier.config.ts
@@ -0,0 +1,22 @@
+import type { PluginConfig } from '@trivago/prettier-plugin-sort-imports'
+import type { Options } from 'prettier'
+
+type FinalOptions = Options & PluginConfig
+
+export default {
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^#lib/(.*)$',
+    '^#components/(.*)$',
+    '^#/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+} satisfies FinalOptions
---
+++ packages/prettier-config/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['prettier.config.ts'],
+  format: ['cjs', 'esm'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  cjsInterop: true,
+})
