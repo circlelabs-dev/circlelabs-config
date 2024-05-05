55ff6b91b9f4d1632c834b57b6ff4609b6bc0094
Jonathan Heneis <jonathan.heneis@circlelabs.dev>
2024-05-05 17:33:16 +0200

    fix(prettier-config-astro): remove dead code

--- packages/prettier-config-astro/tsup.config.ts
+++ packages/prettier-config-astro/tsup.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'tsup'
-// @ts-expect-error: somehow the flag from the tsconfig.json does not matter
-import macros from 'unplugin-macros/esbuild'
 
 export default defineConfig({
-  esbuildPlugins: [macros()],
   entry: ['prettier.config.ts'],
   format: ['cjs', 'esm'],
   splitting: false,
